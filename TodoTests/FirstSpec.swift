338c883ccbff25797c2bc7e2db45faf08df3c3b6
Thiago Lioy <thiagolioy@gmail.com>
2017-05-30 22:05:14 -0300

    Added Carthage Support. Refactoring Initial Dummy Test to use Quick and Nimble

---
+++ TodoTests/FirstSpec.swift
@@ -0,0 +1,21 @@
+//
+//  TodoTests.swift
+//  TodoTests
+//
+//  Created by Thiago Lioy on 30/05/17.
+//  Copyright © 2017 Thiago Lioy. All rights reserved.
+//
+
+import XCTest
+@testable import Todo
+
+import Quick
+import Nimble
+
+class FirstSpec: QuickSpec {
+    override func spec() {
+        it("should pass") {
+            expect(true).to(beTrue())
+        }
+    }
+}
